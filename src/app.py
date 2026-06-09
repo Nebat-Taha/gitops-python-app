de85e508f4c895356376dd62dea9cf0c9b6a7fe9
Nebat-Taha <nebatn@gmail.com>
2026-06-09 18:02:02 -0400

    June 09-6

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ app = FastAPI(title="GitOps Delivery App")
 @app.get("/")
 def read_root():
     return {
-        "message": "Hello, GitOps is awesome and up!",
+        "message": "Hello, Automated GitOps is awesome and up!",
         "hostname": socket.gethostname(),
         "environment": os.getenv("APP_ENV", "development")
     }
